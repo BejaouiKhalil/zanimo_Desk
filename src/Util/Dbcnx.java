b78f7659be8e4c159bd5f350ecaebe41434f8100
Unknown <khalil.bejaoui@esprit.tn>
2018-03-11 13:34:24 +0100

    init project

---
+++ src/Util/Dbcnx.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Util;
+
+import java.sql.Connection;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Dbcnx {
+    String url="";
+    String login="";
+    String pwd="";
+    Connection cnx;
+    private Dbcnx instance;
+    
+   
+    
+}
